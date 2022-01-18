45c796db9985b7cb216da37e6e1c3b9ae36f737e
Evelyn <evelynarrudasilva@gmail.com>
2022-01-18 16:19:16 -0300

    Login adicionado

--- TechSolutio/src/app/app.module.ts
+++ TechSolutio/src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule} from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ListaProdutosComponent } from './lista-produtos/lista-produtos.component';
+import { CadastroProdutosComponent } from './cadastro-produtos/cadastro-produtos.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ListaProdutosComponent,
+    CadastroProdutosComponent,
+    
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
